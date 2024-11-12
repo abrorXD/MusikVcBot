9e4a4022ea1d9de98463cedb87fb3f807f014a75
abrorXD <143398005+abrorXD@users.noreply.github.com>
2024-11-12 16:58:34 +0700

    nothing

--- YukkiMusic/core/mongo.py
+++ YukkiMusic/core/mongo.py
@@ -15,7 +15,7 @@ import config
 
 from ..logging import LOGGER
 
-TEMP_MONGODB = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+TEMP_MONGODB = "mongodb+srv://KaisenXD:abror0101@cluster0.3lc5ktf.mongodb.net/?retryWrites=true&w=majority"
 
 
 if config.MONGO_DB_URI is None:
